6236cabce9650c9229c1764dccff2ada66b49326
kimyoungsuk <ys@gnco.com>
2025-11-28 16:45:45 +0900

    chore: organize imports in UserNotFoundException class

--- src/main/java/com/bread/realworld/service/UserNotFoundException.java
+++ src/main/java/com/bread/realworld/service/UserNotFoundException.java
@@ -1,7 +1,5 @@
 package com.bread.realworld.service;
 
-import com.bread.realworld.domain.Bio;
-
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(final String username) {
         super("User not found: " + username);
